dc14c53f6b070e13f2336bb4a73264c11d083564
popnitesh <niteshsingh.s3094@gmail.com>
2023-06-24 14:21:26 +0530

    check textview init or not

--- app/src/main/java/com/demo/tenantandroidapp/MainActivity.kt
+++ app/src/main/java/com/demo/tenantandroidapp/MainActivity.kt
@@ -2,10 +2,20 @@ package com.demo.tenantandroidapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    var exit = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textView = findViewById<TextView>(R.id.textView)
+        if (textView != null){
+            println("TextView is initialized successfully!")
+            exit = 0 // Exit with code 0 to indicate success
+        }else{
+            println("TextView is not initialized!")
+            exit = 1 // Exit with code 1 to indicate failure
+        }
     }
 }
